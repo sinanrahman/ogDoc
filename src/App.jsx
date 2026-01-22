020a8b3934cb07952d32896476555e1446787e0d
Ranfees <153532316+Ranfees@users.noreply.github.com>
2026-01-22 13:16:08 +0530

    blog view page ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
+import BlogView from "./pages/BlogView" 
 function App() {
 
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-    </>
+   
+  <BlogView/>
+  </>
   )
 }
 
